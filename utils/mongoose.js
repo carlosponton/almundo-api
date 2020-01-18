ff8f07eb9fa638e87d0770949129d0e41393ad97
Carlos Pontón <carlos.ponton@coraltech.co>
2020-01-18 00:54:12 -0500

    string de conexión

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -1,3 +1,3 @@
 // getting-started.js
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://carlosponton:Coraltech2019@cluster0-zeamw.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
+mongoose.connect(process.env.CONNECTION_STRING || '', {useNewUrlParser: true});
